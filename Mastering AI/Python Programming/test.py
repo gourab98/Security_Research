bc584799c21b65602347b03ee0ff60841b527485
gourab98 <gourabsahag@gmail.com>
2025-04-02 11:18:00 +0600

    added a new python code

--- Mastering AI/Python Programming/test.py
+++ Mastering AI/Python Programming/test.py
@@ -1,7 +1,14 @@
-input = "Four score and seven years ago"
+def is_power_of(number, base):
+ # Base case: when number is smaller than base.
+    if number < base:
+        # If number is equal to 1, it's a power (base**0).
+        return False
+    elif(number==base) : return True
 
-for c in input:
-  if c.lower() in ['a', 'e', 'i', 'o', 'u']:
-    print(c)
+    # Recursive case: keep dividing number by base.
+    return is_power_of(number/base, base)
 
-print([c for c in input if c.lower() in ['a', 'e', 'i', 'o', 'u']])
+
+print(is_power_of(8,2))     # Should be True
+print(is_power_of(64,4))    # Should be True
+print(is_power_of(70,10))   # Should be False
