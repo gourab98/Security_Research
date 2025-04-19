cf5d4aeed7c05cbaa50ffa43ceaea44e5c2e9807
gourab98 <gourabsahag@gmail.com>
2025-04-19 15:25:37 +0600

    added some files

---
+++ AI/Python Programming/list.py
@@ -0,0 +1,8 @@
+fruits = ["Pineapple", "Banana", "Apple", "Melon"]
+fruits.append("Kiwi")
+fruits.insert(0, "Orange")
+fruits.insert(25, "Peach")
+fruits.remove("Melon")
+fruits.pop(3)
+fruits[2] = "Strawberry"
+print(fruits)
