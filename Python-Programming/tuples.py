4d78a0de39f53f5e433db3ca3d130a2104d96abd
gourab98 <gourabsahag@gmail.com>
2025-04-19 19:21:19 +0600

    remaining files

---
+++ Python-Programming/tuples.py
@@ -0,0 +1,12 @@
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+
+result = convert_seconds(5000)
+print(type(result))
+
+hours, minutes, seconds = result
+
+print("Hour:" , hours , " Minute:" , minutes , " Second:" , seconds)
