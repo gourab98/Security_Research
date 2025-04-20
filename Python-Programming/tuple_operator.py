0cc4d1f5a40e84036ca6ad3f8cac5f35df7c69fe
gourab98 <gourabsahag@gmail.com>
2025-04-20 11:10:53 +0600

    tuple python files

---
+++ Python-Programming/tuple_operator.py
@@ -0,0 +1,7 @@
+#Convert list in to tuple
+my_list = [1,2,3,4,5,6]
+my_tuple = tuple(my_list)
+print(type(my_tuple), "Tuple : ", my_tuple)
+
+tuple_list = 1,2,3,4,5
+print(type(tuple_list), "Tuple list: ", tuple_list)
