4ca3daf13810dfd952825cc6237f1929d4c39977
gourab98 <gourabsahag@gmail.com>
2025-05-03 23:53:58 +0600

    added sort functions

---
+++ Python-Programming/sort.py
@@ -0,0 +1,11 @@
+### Sort
+numbers= [6,4,8,2,1,7]
+numbers.sort()
+print(numbers)
+
+### Sorted
+names = ["Carlos","Alex","Ray","Ketthy"]
+print(names)
+print(sorted(names))
+print(names)
+print(sorted(names,key=len))
